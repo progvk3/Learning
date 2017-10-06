8b4d5bcf9824504045431f2143c374cb4fa7c36c
progvk3 <32221403+progvk3@users.noreply.github.com>
2017-10-06 16:51:37 +0530

    Creating new doc on classes

---
+++ Scala/classes_objects.scala
@@ -0,0 +1,14 @@
+import java.io._
+class vasanth_testing {
+  def hi(str: String){
+    println ("Hi Vasanth")
+  }
+}
+
+
+object demo{
+  def main(args:Array[String]){
+    var a = new vasanth_testing()
+    a.hi("Vasanth")
+  }
+}
