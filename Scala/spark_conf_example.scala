fddb6ad1791d47e97f6f32d1a2ecc005c1b575d9
progvk3 <32221403+progvk3@users.noreply.github.com>
2017-10-07 23:03:05 +0530

    Hi

--- Scala/spark_conf_example.scala
+++ Scala/spark_conf_example.scala
@@ -2,7 +2,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
 val conf = new SparkConf().setAppName("vasanth_learning").setMaster("yarn-client")
-val sc = SparkContext(conf)
+val sc = new SparkContext(conf)
 
 val lines = sc.textFile("/user/testing/my_first_program.scala")
 println(lines.count)
