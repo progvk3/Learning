f60c7bfd50c45102ffe020eb531ea01d7733be0e
progvk3 <32221403+progvk3@users.noreply.github.com>
2017-10-07 23:24:30 +0530

    Hi

--- Scala/spark_conf_example.scala
+++ Scala/spark_conf_example.scala
@@ -3,7 +3,7 @@
 
 //val conf = new SparkConf().setAppName("vasanth_learning").setMaster("yarn-client")
 //val sc = new SparkContext(conf)
-sc.setLogLevel(WARN)
+sc.setLogLevel("WARN")
 val lines = sc.textFile("/user/testing/my_first_program.scala")
 println(lines.count)
 println(lines.first())
