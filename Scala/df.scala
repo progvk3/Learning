df079f1f83dfe0707ac41fe18f64f88199453bbd
progvk3 <32221403+progvk3@users.noreply.github.com>
2017-10-08 16:52:38 +0530

    Hi

--- Scala/df.scala
+++ Scala/df.scala
@@ -1,3 +1,7 @@
-val df = sc.read.csv("/Spark DataFrames/CitiGroup2006_2008")
+import org.apache.spark.sql.SparkSession
+
+val spark = SparkSession.builder().getOrCreate()
+
+val df = spark.read.csv("/Spark DataFrames/CitiGroup2006_2008")
 
 df.head(5)
