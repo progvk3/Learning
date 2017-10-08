712863719bc651b3548ff3c187517884c596aa9c
progvk3 <32221403+progvk3@users.noreply.github.com>
2017-10-08 21:33:34 +0530

    Hi

--- Scala/df_agg_func.scala
+++ Scala/df_agg_func.scala
@@ -2,7 +2,7 @@ import org.apache.spark.sql.SparkSession
 
 val spark = SparkSession.builder().getOrCreate()
 
-val df_t = spark.read.option("header",true).option("inferSchema",true).csv("file:///home/vasanth/Learning/Scala/Spark DataFrames/Sales.csv")
+val df = spark.read.option("header",true).option("inferSchema",true).csv("file:///home/vasanth/Learning/Scala/Spark DataFrames/Sales.csv")
 
 
 df.printSchema()
