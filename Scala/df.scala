11c2fe694b1ee19e06398fa5846b823a073eb422
progvk3 <32221403+progvk3@users.noreply.github.com>
2017-10-08 17:19:49 +0530

    Hi

--- Scala/df.scala
+++ Scala/df.scala
@@ -4,7 +4,7 @@ val spark = SparkSession.builder().getOrCreate()
 
 val df = spark.read.option("header",true).option("inferSchema",true).csv("file:///home/vasanth/Learning/Scala/Spark DataFrames/CitiGroup2006_2008")
 
-df.head(6)
+//df.head(6)
 
 
 for(row <- df.head(5)){
