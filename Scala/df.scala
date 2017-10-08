8b4cb1473fb098b6e6a0b3fd6830a41ad726f11d
progvk3 <32221403+progvk3@users.noreply.github.com>
2017-10-08 17:19:35 +0530

    Hi

--- Scala/df.scala
+++ Scala/df.scala
@@ -2,7 +2,7 @@ import org.apache.spark.sql.SparkSession
 
 val spark = SparkSession.builder().getOrCreate()
 
-val df = spark.read.option("header",true).csv("file:///home/vasanth/Learning/Scala/Spark DataFrames/CitiGroup2006_2008")
+val df = spark.read.option("header",true).option("inferSchema",true).csv("file:///home/vasanth/Learning/Scala/Spark DataFrames/CitiGroup2006_2008")
 
 df.head(6)
 
