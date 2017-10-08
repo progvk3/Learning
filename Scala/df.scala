fc8b833433c743ad69a4824aeca516b8506834ab
progvk3 <32221403+progvk3@users.noreply.github.com>
2017-10-08 17:16:35 +0530

    Hi

--- Scala/df.scala
+++ Scala/df.scala
@@ -2,6 +2,11 @@ import org.apache.spark.sql.SparkSession
 
 val spark = SparkSession.builder().getOrCreate()
 
-val df = spark.read.csv("file:///home/vasanth/Learning/Scala/Spark DataFrames/CitiGroup2006_2008")
+val df = spark.read.option("header",true).csv("file:///home/vasanth/Learning/Scala/Spark DataFrames/CitiGroup2006_2008")
 
 df.head(6)
+
+
+for(line <- df.head(5)){
+  println(row)
+}
