c5e88290449fca7c81663f890624473ca4e23725
progvk3 <32221403+progvk3@users.noreply.github.com>
2017-10-09 20:33:09 +0530

    Hi

--- Scala/wordcount.scala
+++ Scala/wordcount.scala
@@ -3,6 +3,6 @@ import spark.implicits._
 
 val spark = SparkSession.builder().getOrCreate()
 
-val wc = sc.textFile("file:///home/vasanth/Learning/Scala/my_first_program.scala").Map(line => line.split(" ")).flatMap(map => (map,1)).reduceByKey(_ + _)
+val wc = sc.textFile("file:///home/vasanth/Learning/Scala/my_first_program.scala").map(line => line.split(" ")).flatMap(map => (map,1)).reduceByKey(_ + _)
 
 wc.show()
